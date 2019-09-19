ee5dfd61ccfe71036e51bc1d745fef781708bd85
Yousef Al Othman <Yousef@MacBook-Pro-2.local>
2019-09-19 10:56:01 +0300

    My solution

--- src/App.js
+++ src/App.js
@@ -8,13 +8,45 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: "",
+    italic: "",
+    underline: ""
+  };
+
+  styleHandler = style => {
+    if (style === "bold") {
+      if (this.state.bold === "") {
+        this.setState({ bold: "bold" });
+      } else {
+        this.setState({ bold: "" });
+      }
+    } else if (style === "italic") {
+      if (this.state.italic === "") {
+        this.setState({ italic: "italic" });
+      } else {
+        this.setState({ italic: "" });
+      }
+    } else if (style === "underline") {
+      if (this.state.underline === "") {
+        this.setState({ underline: "underline" });
+      } else {
+        this.setState({ underline: "" });
+      }
+    }
+  };
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.styleHandler(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +57,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.setState({ color: color })}
         />
       );
     });
@@ -32,7 +65,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold,
+            fontStyle: this.state.italic,
+            textDecorationLine: this.state.underline
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
